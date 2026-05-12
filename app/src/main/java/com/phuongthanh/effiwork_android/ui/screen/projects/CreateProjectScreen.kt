572b81c2bd540d15a318f882afb7cb1d84b0d956
PhamThiPhuongThanh36 <phamthiphuongthanh36@gmail.com>
2026-05-12 08:45:38 +0700

    fix(CreateProjectScreen): update import paths for ProjectsIntent and ProjectsViewModel

--- app/src/main/java/com/phuongthanh/effiwork_android/ui/screen/projects/CreateProjectScreen.kt
+++ app/src/main/java/com/phuongthanh/effiwork_android/ui/screen/projects/CreateProjectScreen.kt
@@ -8,10 +8,9 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
-import androidx.hilt.navigation.compose.hiltViewModel
 import com.phuongthanh.effiwork_android.R
-import com.phuongthanh.effiwork_android.viewmodel.projects.ProjectsIntent
-import com.phuongthanh.effiwork_android.viewmodel.projects.ProjectsViewModel
+import com.phuongthanh.effiwork_android.viewmodel.project.ProjectsIntent
+import com.phuongthanh.effiwork_android.viewmodel.project.ProjectsViewModel
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
